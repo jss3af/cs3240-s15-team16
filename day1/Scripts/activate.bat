ef34195c0b26bfacf11e830af2a7137b55e34efc
Joseph <jcu5df@virginia.edu>
2015-03-02 16:09:34 -0500

    Initial Commit

---
+++ day1/Scripts/activate.bat
@@ -0,0 +1,26 @@
+@echo off
+set "VIRTUAL_ENV=d:\Documents\2ndYear_SecondSemester\CS3240\cs3240-s15-team16\day1"
+
+if defined _OLD_VIRTUAL_PROMPT (
+    set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
+) else (
+    if not defined PROMPT (
+        set "PROMPT=$P$G"
+    )
+	set "_OLD_VIRTUAL_PROMPT=%PROMPT%"	
+)
+set "PROMPT=(day1) %PROMPT%"
+
+if not defined _OLD_VIRTUAL_PYTHONHOME (
+    set "_OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%"
+)
+set PYTHONHOME=
+
+if defined _OLD_VIRTUAL_PATH (
+    set "PATH=%_OLD_VIRTUAL_PATH%"
+) else (
+    set "_OLD_VIRTUAL_PATH=%PATH%"
+)
+set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
+
+:END
